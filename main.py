3d8d24700c5a0828e72db579a3dcff25b3dfc89a
Elias Jegervatn Severinsen <elias.jegervatn.severinsen@nmbu.no>
2024-12-28 22:52:02 +0100

    Added new example

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from examples.heat import heat2D_Neumann_example, heat2D_Dirichlet_example
+from examples.heat import heat2D_Neumann_example, heat2D_Dirichlet_example, heat1D_Dirichlet_example
 from examples.wave import wave2D_Dirichlet_example
 from examples.poisson import poisson2D_Dirichlet_example
 
@@ -10,3 +10,5 @@ if __name__ == '__main__':
     #heat2D_Dirichlet_example()
     wave2D_Dirichlet_example()
     #poisson2D_Dirichlet_example()
+    #heat1D_Dirichlet_example()
+    
