1910f0b04a24df8bdbe27e5f3192ac4614e91318
Elias Jegervatn Severinsen <elias.jegervatn.severinsen@nmbu.no>
2024-12-30 19:36:50 +0100

    Import models

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import numpy as np
+from src.mesh import Mesh
+from src.fem import Fem1D, Fem2D
+from src.graphics import MeshGraphics1D, MeshGraphics2D
+
+# examples
 from examples.heat import heat2D_Neumann_example, heat2D_Dirichlet_example, heat1D_Dirichlet_example
 from examples.wave import wave2D_Dirichlet_example
 from examples.poisson import poisson2D_Dirichlet_example
@@ -11,4 +17,5 @@ if __name__ == '__main__':
     wave2D_Dirichlet_example()
     #poisson2D_Dirichlet_example()
     #heat1D_Dirichlet_example()
+
     
